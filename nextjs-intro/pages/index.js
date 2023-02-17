0d8819512a3496c393940b8e5e5c3d409252f49c
imserimkim <tpfla22@naver.com>
2023-02-17 13:45:33 +0900

    commit

---
+++ nextjs-intro/pages/index.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+export default function Home() {
+    const [counter, setCounter] = useState(0);
+    return  <div>
+                <h1>hello {counter} </h1>
+                <button onClick={()=> setCounter((prev) => prev + 1) }>+</button>
+            </div>; 
+
+}
+
